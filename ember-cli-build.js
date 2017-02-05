d51be8fde351cb2a05b0e3d628f5daca89e6eb38
Chandler Kent <chandler.kent@gmail.com>
2017-02-05 17:35:10 -0500

    Upgrades Ember to 2.11.0

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    // Add options here
   });
 
   app.import('bower_components/normalize-css/normalize.css');
