4c829af6e8924e00f2e02a8bd16edc205491d99c
Chandler Kent <chandler.kent@aprimo.com>
2022-02-10 23:17:06 -0500

    Change to ember-local-storage, but it does not work.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,1 @@
-import LSAdapter from 'ember-localstorage-adapter';
-
-export default LSAdapter.extend({
-  namespace: 'math-facts'
-});
+export { default } from 'ember-local-storage/adapters/local';
