f0d8b6ba1e32f1cae34a131bdbd58c71b052f1e8
Chandler Kent <chandler.kent@gmail.com>
2022-02-21 19:07:12 -0600

    Add option to print mastered facts. (#17)
    Co-authored-by: Chandler Kent <chandler.kent@aprimo.com>

--- app/controllers/interview/summary.js
+++ app/controllers/interview/summary.js
@@ -1,78 +1,85 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Controller.extend({
   chart: null,
   results: null,
+  printMasteredFacts: false,
 
   actions: {
     clickCell(row, column) {
-      let chart = this.get('chart');
+      let chart = this.get("chart");
 
       let cell = chart[row][column];
 
-      if (!cell.get('isInteractive')) {
+      if (!cell.get("isInteractive")) {
         return;
       }
 
-      cell.toggleProperty('isOverridden');
+      cell.toggleProperty("isOverridden");
     },
 
     clickColumn(column) {
-      let chart = this.get('chart');
+      let chart = this.get("chart");
 
       let active;
       for (let i = 0; i < chart.length; i++) {
         let cell = chart[i][column];
-        if (!cell.get('isInteractive')) {
+        if (!cell.get("isInteractive")) {
           continue;
         }
 
         if (active === undefined) {
-          active = !!!cell.get('isOverridden');
+          active = !!!cell.get("isOverridden");
         }
 
-        cell.set('isOverridden', active);
+        cell.set("isOverridden", active);
       }
     },
 
     clickRow(row) {
-      let chart = this.get('chart');
+      let chart = this.get("chart");
 
       let columns = chart[0].length;
       let active;
       for (let i = 0; i < columns; i++) {
         let cell = chart[row][i];
-        if (!cell.get('isInteractive')) {
+        if (!cell.get("isInteractive")) {
           continue;
         }
 
         if (active === undefined) {
-          active = !!!cell.get('isOverridden');
+          active = !!!cell.get("isOverridden");
         }
 
-        cell.set('isOverridden', active);
+        cell.set("isOverridden", active);
       }
     },
 
     toggleAll() {
-      let chart = this.get('chart');
+      let chart = this.get("chart");
 
       let rows = chart.length;
       let active;
       for (let row = 0; row < rows; row++) {
         for (let column = 0; column < chart[row].length; column++) {
           let cell = chart[row][column];
-          if (!cell.get('isInteractive')) {
+          if (!cell.get("isInteractive")) {
             continue;
           }
 
           if (active === undefined) {
-            active = !!!cell.get('isOverridden');
+            active = !!!cell.get("isOverridden");
           }
 
-          cell.set('isOverridden', active);
+          cell.set("isOverridden", active);
         }
       }
-    }
-  }
+    },
+
+    print(event) {
+      event.preventDefault();
+
+      window.print();
+    },
+  },
 });
